c2099bf029f26aa237cc365289d7ba3488ebea21
Igor Martins <igormartins0301@gmail.com>
2025-03-09 18:54:04 -0300

    feat: incluindo primeiro arquivo tf

--- provider.tf
+++ provider.tf
@@ -12,7 +12,7 @@ provider "aws" {
   region = "us-east-1"
 
   assume_role {
-    role_arn = "arn:aws:iam::443370712526:role/principal-terraform-project"
+    role_arn     = "arn:aws:iam::443370712526:role/bootcamp-cloud-terraform"
     session_name = "github-actions"
   }
 }
