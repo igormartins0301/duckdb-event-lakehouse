6a9536bf60996ae5ec9fefb943ebbed9aa50cbf9
Igor Martins <igormartins0301@gmail.com>
2025-03-09 18:31:30 -0300

    feat: incluindo primeiro arquivo tf

--- budget.tf
+++ budget.tf
@@ -1,22 +1,13 @@
-resource "aws_budgets_budget" "monthly_budget" {
-  name = "terraform-monthly-budget"
-  time_unit = "MONTHLY"
-  budget_type = "COST"
-  limit {
-    amount = 10.00
-    unit   = "USD"
-  }
-  cost_types {
-    include_tax = false
-    include_subscription = true
-  }
+resource "aws_budgets_budget" "cost" {
+  name         =  "budget-free-tier"
+  budget_type  = "COST"
+  limit_amount = "5"
+  limit_unit   = "USD"
   notification {
-    comparison_operator = "GREATER_THAN"
-    threshold = 1
-    notification_type = "ACTUAL"
-    subscribers {
-      subscription_type = "EMAIL"
-      address = "igormartins0301@gmail.com"
-    }
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 100
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "FORECASTED"
+    subscriber_email_addresses = ["igormartins0301@gmail.com"]
   }
 }
