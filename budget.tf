d919050b1add25dc1900e3cd47f15de0533e080c
Igor Martins <igormartins0301@gmail.com>
2025-03-09 18:32:51 -0300

    feat: incluindo primeiro arquivo tf

--- budget.tf
+++ budget.tf
@@ -3,6 +3,7 @@ resource "aws_budgets_budget" "cost" {
   budget_type  = "COST"
   limit_amount = "5"
   limit_unit   = "USD"
+  time_unit    = "MONTHLY"
   notification {
     comparison_operator        = "GREATER_THAN"
     threshold                  = 100
